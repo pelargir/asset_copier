0fbe72924ada794fc98d67092295184a2f197638
Matthew Bass <matt@anacreon.local>
2008-10-31 10:16:40 -0400

    added dir to manifest

--- asset_copier_generator.rb
+++ asset_copier_generator.rb
@@ -1,6 +1,7 @@
 class AssetCopierGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
+      m.directory File.join("vendor/plugins/#{file_name}/tasks")
       m.file "install.rb", "vendor/plugins/#{file_name}/install.rb"
       m.file "asset_copier.rake", "vendor/plugins/#{file_name}/tasks/asset_copier.rake"
     end
