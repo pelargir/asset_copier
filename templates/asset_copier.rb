11203bec6cd57aa7cc5664db80272544a13c35c6
Matthew Bass <matt@anacreon.local>
2008-11-03 11:55:15 -0500

    hooked init.rb to copy files when Rails starts in development mode

--- templates/asset_copier.rb
+++ templates/asset_copier.rb
@@ -2,6 +2,7 @@
 # http://github.com/pelargir/asset_copier
 
 require 'find'
+require 'digest/md5'
 
 class AssetCopier
   @source = File.expand_path(File.join(File.dirname(__FILE__), '..', 'files'))
@@ -31,6 +32,30 @@ class AssetCopier
       raise e
     end
     print_deletion_warnings
+  end
+  
+  def self.warn(plugin_name)
+    self.plugin_name = plugin_name
+    paths.each do |path| 
+      dest_path = path.gsub(source, destination)
+      if File.directory?(path)
+        puts "WARNING: Directory #{dest_path} is required by the #{plugin_name} " <<
+          "plugin but does not exist" unless File.exists?(dest_path)
+      else
+        if File.exists?(dest_path)
+          puts "WARNING: #{dest_path} is out of date and needs to be reinstalled " <<
+            "from the #{plugin_name} plugin" unless compare(path, dest_path)
+        else
+          puts "WARNING: #{dest_path} is missing and needs to be installed " <<
+            "from the #{plugin_name} plugin"
+        end
+      end
+    end
+    print_deletion_warnings
+  end
+  
+  def self.compare(file1, file2)
+    Digest::MD5.hexdigest(File.read(file1)) == Digest::MD5.hexdigest(File.read(file2))
   end
   
   def self.print_deletion_warnings
