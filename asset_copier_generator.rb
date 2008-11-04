b1cf8acd38b05106d5baab820f38d3606cc3575d
Matthew Bass <matt@anacreon.local>
2008-11-03 22:01:28 -0500

    stuck asset_copier.rb in its own namespace

--- asset_copier_generator.rb
+++ asset_copier_generator.rb
@@ -2,12 +2,13 @@ class AssetCopierGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       m.directory "vendor/plugins/#{file_name}/tasks"
-
-      m.template "init.erb", "vendor/plugins/#{file_name}/init.rb"
-      m.template "install.erb", "vendor/plugins/#{file_name}/install.rb"
-      m.template "asset_copier.erb", "vendor/plugins/#{file_name}/tasks/asset_copier.rake"
-
-      m.file "asset_copier.rb", "vendor/plugins/#{file_name}/lib/asset_copier.rb"
+      m.directory "vendor/plugins/#{file_name}/lib/#{file_name}"
+      
+      m.template "init.rb.erb", "vendor/plugins/#{file_name}/init.rb"
+      m.template "install.rb.erb", "vendor/plugins/#{file_name}/install.rb"
+      m.template "asset_copier.rake.erb", "vendor/plugins/#{file_name}/tasks/asset_copier.rake"
+      m.template "asset_copier.rb.erb", "vendor/plugins/#{file_name}/lib/#{file_name}/asset_copier.rb"
+      
       m.file "deleted_files", "vendor/plugins/#{file_name}/deleted_files"
     end
   end
